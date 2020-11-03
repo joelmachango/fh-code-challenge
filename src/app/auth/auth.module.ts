e5f6d2498e4a9190ce056f4f1ff533cd279cde73
Joel Machango <joelmachango@gmail.com>
2020-11-03 16:26:53 +0300

    Auth routing

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { AuthComponent } from './auth.component';
 
 
+const routes: Routes = [
+  {
+    path: "",
+    component: AuthComponent,
+    children: [
+      { path: "register", component: RegisterComponent },
+      { path: "login", component: LoginComponent }
+    ]
+  }
+]
 
 @NgModule({
-  declarations: [RegisterComponent, LoginComponent],
+  declarations: [RegisterComponent, LoginComponent, AuthComponent],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
   ]
 })
