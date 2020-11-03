dd58813745c57fcca96391f23eea3c038edc5b03
Joel Machango <joelmachango@gmail.com>
2020-11-03 22:10:00 +0300

    Login screen UI

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  errors: any[] = [];
+  notifyMessage: string = "";
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.initForm();
   }
 
+  initForm() {
+    this.loginForm = this.fb.group({
+      email: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern(
+            "^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$"
+          ),
+        ],
+      ],
+      password: ["", Validators.required],
+    });
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+  }
 }
