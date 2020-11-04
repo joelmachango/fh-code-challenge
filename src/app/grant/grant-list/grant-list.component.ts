58099f4ff774a498c820fa0fefd30b9115a03f06
Joel Machango <joelmachango@gmail.com>
2020-11-04 21:08:35 +0300

    Clean up grant list component and create grant model

--- src/app/grant/grant-list/grant-list.component.ts
+++ src/app/grant/grant-list/grant-list.component.ts
@@ -1,17 +1,7 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { MatPaginator } from "@angular/material/paginator";
-import { MatTableDataSource } from "@angular/material/table";
+import { Grant } from "../shared/grant.model";
 
-export interface PeriodicElement {
-  id: number;
-  name: string;
-  status: string;
-  grantor: string;
-  location: string;
-  amount: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
+const ELEMENT_DATA: Grant[] = [
   {
     id: 1,
     status: "In Consideration",
@@ -22,73 +12,9 @@ const ELEMENT_DATA: PeriodicElement[] = [
   },
   {
     id: 2,
-    status: "In Consideration",
-    name: "Helium",
+    status: "Deployment",
+    name: "Provision of primary health services for refugees.",
     grantor: "USAID",
-    location: "Nairobi",
-    amount: "$15,00,000",
-  },
-  {
-    id: 3,
-    status: "In Consideration",
-    name: "Lithium",
-    grantor: "USAID",
-    location: "Nairobi",
-    amount: "$15,00,000",
-  },
-  {
-    id: 4,
-    status: "In Consideration",
-    name: "Beryllium",
-    grantor: "USAID",
-    location: "Nairobi",
-    amount: "$15,00,000",
-  },
-  {
-    id: 5,
-    status: "In Consideration",
-    name: "Boron",
-    grantor: "USAID",
-    location: "Nairobi",
-    amount: "$15,00,000",
-  },
-  {
-    id: 6,
-    status: "In Consideration",
-    name: "Carbon",
-    grantor: "USAID",
-    location: "Nairobi",
-    amount: "$15,00,000",
-  },
-  {
-    id: 7,
-    status: "In Consideration",
-    name: "Nitrogen",
-    grantor: "USAID",
-    location: "Nairobi",
-    amount: "$15,00,000",
-  },
-  {
-    id: 8,
-    status: "In Consideration",
-    name: "Oxygen",
-    grantor: "USAID",
-    location: "Nairobi",
-    amount: "$15,00,000",
-  },
-  {
-    id: 9,
-    status: "In Consideration",
-    name: "Fluorine",
-    grantor: "USAID",
-    location: "Nairobi",
-    amount: "$15,00,000",
-  },
-  {
-    id: 10,
-    status: "In Consideration",
-    name: "Neon",
-    grantor: "USAIDEEEEE",
     location: "Nairobi",
     amount: "$15,00,000",
   },
@@ -101,8 +27,6 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class GrantListComponent implements OnInit {
   constructor() {}
-
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   ngOnInit() {}
 
