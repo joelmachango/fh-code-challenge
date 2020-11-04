43083e4360e8aab82e52ed4a9755a6a052b8e47c
Joel Machango <joelmachango@gmail.com>
2020-11-05 00:07:25 +0300

    Update migrations and fix SQLSTATE[42S01] error

--- laravel_server/database/migrations/2020_11_04_202627_create_user_table.php
+++ laravel_server/database/migrations/2020_11_04_202627_create_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGrantTable extends Migration
+class CreateUserTable extends Migration
 {
   /**
    * Run the migrations.
@@ -13,10 +13,13 @@ class CreateGrantTable extends Migration
    */
   public function up()
   {
-    Schema::create('grants', function (Blueprint $table) {
+    Schema::create('users', function (Blueprint $table) {
       $table->increments('id');
       $table->timestamps();
-      $table->text('name');
+      $table->string('name');
+      $table->string('email')->unique();
+      $table->string('password');
+      $table->rememberToken();
     });
   }
 
@@ -27,6 +30,6 @@ class CreateGrantTable extends Migration
    */
   public function down()
   {
-    Schema::dropIfExists('grant');
+    Schema::dropIfExists('user');
   }
 }
